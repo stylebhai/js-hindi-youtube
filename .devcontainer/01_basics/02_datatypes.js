e1190da0b38b2321d8bfd7b61bb352332e4ef22b
stylebhai <152050769+stylebhai@users.noreply.github.com>
2024-02-02 19:02:37 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJS, not browser
+
+console.log(3
+    +
+    3);// code readability should be high
+
+    let name = "Hitesh"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+//primitive datatypes
+    //number => 2 to the power 53//
+    //bigint
+    //string => ""
+    //boolean => true/false
+    //null => standalone value (it is an object) i.e typeof = object
+    //undefined => typeof = undefined
+    //symbol => unique
+
+ // Object  
+   
+   console.log(typeof null); // object
+   console.log(typeof undefined); // undefined
