e927d614c3f4abb225711b4df90a18536dbe2952
stylebhai <152050769+stylebhai@users.noreply.github.com>
2024-02-03 06:30:11 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversions.js
@@ -0,0 +1,25 @@
+let score = "hitesh";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// true => 1; false => 0
+// "33abc" => NaN
+
+let isLoggedIn = "hitesh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"hitesh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
