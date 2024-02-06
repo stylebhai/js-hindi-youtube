628ae0b661e7b8cc9ac2df4b8cdeae46328194ea
stylebhai <152050769+stylebhai@users.noreply.github.com>
2024-02-06 14:20:57 +0000

    summarized-datatypes

--- .devcontainer/01_basics/datatype-summary.js
+++ .devcontainer/01_basics/datatype-summary.js
@@ -1,5 +1,31 @@
 // Primitive datatype
 // 7 types-  String,Boolean,Number,null,undefined,BigInt,Symbol
 
+
+
+const temp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+//console.log(id);
+//console.log(anotherId);
+//console.log(id === anotherId);
+
+const bigNumber = 4512378542223658n;
+
 // Reference datatype(Non Primitive)
 // 3 types-  Arrays,Objects,Functions
+
+const heroes = ["shaktiman", "nagraj","juniorG"];
+let myObj = {
+    name: "visvanath",
+    age: 47
+}
+
+const myFunction = function(){
+  console.log("hello world!");
+}
+
+console.log(typeof id);
