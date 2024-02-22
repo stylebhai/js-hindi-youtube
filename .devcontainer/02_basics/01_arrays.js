d9459f82b94e0e9c9d2c2e731376a3997e2c5292
stylebhai <152050769+stylebhai@users.noreply.github.com>
2024-02-22 10:23:34 +0000

    learnArrays

--- .devcontainer/02_basics/01_arrays.js
+++ .devcontainer/02_basics/01_arrays.js
@@ -5,3 +5,31 @@ const myArr = [0,1,2,3,4,5,"red",true]
 //console.log(myArr[4]);
 
 const myArr2 = new Array(1,2,3,4);
+//console.log(myArr2);
+
+//myArr.push(6)
+//myArr.shift()
+//myArr.unshift(9)
+//myArr.pop()
+//console.log(myArr.includes(9))
+//console.log(myArr.indexOf(21))
+//console.log(myArr.indexOf(4))
+//console.log(myArr);
+
+const newArr = myArr.join();
+//console.log(myArr);
+//console.log(newArr);
+//console.log(typeof newArr);
+//console.log(typeof myArr);
+
+// Slice, Splice
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("C",myArr);
