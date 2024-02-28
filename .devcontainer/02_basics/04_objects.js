db8a68cae0d7a9c186e0f90d7df8e7cd7161b1a5
stylebhai <152050769+stylebhai@users.noreply.github.com>
2024-02-28 14:42:07 +0000

    objects2

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,36 @@
+ //const tinderUser = new Object()      //singleton object
+const tinderUser = {}; //non-singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "some@google.com",
+    fullName: {
+        userFullname: {
+            firstName: "hitesh",
+            lastName: "choudhary"
+        }
+    }
+}
+
+//console.log(regularUser.fullName);
+//console.log(regularUser.fullName.userFullname)
+//console.log(regularUser.fullName.userFullname.lastName)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+//const obj3 = {obj1,obj2}
+//const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+//console.log(tinderUser);
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser))
+//console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty('isLogged'))
