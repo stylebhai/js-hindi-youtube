42cf524fdaaadb6ca6caa0f1c5468e86d9e1b5e4
stylebhai <152050769+stylebhai@users.noreply.github.com>
2024-03-05 06:19:32 +0000

    scopes

---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,47 @@
+//var c = 300
+let a = 300
+
+if (true){
+    let a = 10
+    const b = 20
+   //console.log("INNER:", a);
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    
+    //two()
+}
+//one()
+
+if(true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+//+++++++++++++++++++++++++ INTERESTING +++++++++++++++++++++//
+
+console.log(addone(5));
+function addone(num){
+    return num + 1;
+}
+
+//addtwo(5)
+const addtwo = function(num){
+    return num + 2
+}
